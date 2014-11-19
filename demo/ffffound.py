9fc64f9bd654607911a17f11b8537114da7c38d1
Mathieu Lecarme <mlecarme@bearstech.com>
2014-11-19 11:27:31 +0100

    Fix: Statsd from Env, more counting.

--- demo/ffffound.py
+++ demo/ffffound.py
@@ -7,18 +7,25 @@ gevent.monkey.patch_all()
 import urllib2
 import feedparser
 
-from statsd import StatsClient
+from statsd.defaults.env import statsd
 
-statsd = StatsClient()
+def wget(url, name=None):
+    if name is None:
+        name= url.split('/')[-1]
+    d = urllib2.urlopen(url)
+    with open(name, 'w') as f:
+        f.write(d.read())
 
 @statsd.timer('slurp')
 def slurp(entry):
-    path = entry.media_content[0]['url']
-    name= path.split('/')[-1]
-    d = urllib2.urlopen(path)
-    with open(name, 'w') as f:
-        f.write(d.read())
-        statsd.incr('image')
+    statsd.incr('workers')
+    for url in [entry.media_content[0]['url'], entry.ffffound_source['url']]:
+        try:
+            wget(url)
+        except :
+            statsd.incr('error')
+    statsd.incr('image')
+    statsd.decr('workers')
 
 ffffound = feedparser.parse('http://feeds.feedburner.com/ffffound/everyone')
 s = []
